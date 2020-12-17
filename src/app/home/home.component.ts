d0123eba603e4a49638fd10a0bea9b836ab568ae
Hubert Milczarek <hb.milczarek@gmail.com>
2020-12-17 15:46:55 +0100

    added IP to user

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { IpServiceService } from '../ip-service.service';  
 
 import { PlayerService } from '../player.service';
 import { Player } from '../player';
@@ -12,12 +13,21 @@ export class HomeComponent implements OnInit {
 
   player: Player;
   players: Player[];
+  ipAddress:string;  
+  error_message:string;
 
-  constructor(private playerService: PlayerService,) { }
+  constructor(private playerService: PlayerService, private ip:IpServiceService) { }
 
   ngOnInit() {
     this.getPlayers();
-  }
+    this.getIP();  
+    }
+
+  getIP(){  
+    this.ip.getIPAddress().subscribe((res:any)=>{  
+      this.ipAddress=res.ip;  
+    });  
+  } 
 
   getPlayers(): void {
     this.playerService.getPlayers()
@@ -26,8 +36,10 @@ export class HomeComponent implements OnInit {
 
   add(name: string): void {
     name = name.trim();
+    let ip = this.ipAddress;
+    let id = this.players.length+1;
     if (!name) { return; }
-    this.playerService.addPlayer({name} as Player)
+    this.playerService.addPlayer({name, ip, id} as Player)
                       .subscribe(player => {this.players.push(player);});
   }
 
--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
 import { catchError } from 'rxjs/operators';
   
 import { Observable, of } from 'rxjs';
