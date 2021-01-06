b8c73dcdcc0b3678c48968f3d47e3917bcd09e68
Kamil Jankowski <1990.kamil.jankowski@gmail.com>
2021-01-06 13:00:05 +0100

    Dual-language functionality added for toast notification on end page

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,13 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { MessageService } from '../messages.service';
-import { ShootMapService } from '../shoot-map.service';
-import { NotificationMessage, NotificationType } from '../notification.message';
 import { NotificationService } from '../notification.service';
-import { Square } from '../square';
 import { TranslateService } from '@ngx-translate/core';
+import { NotificationType } from '../notification.message';
 
 /**
- * Represents landing page to be shown when no more players can be added to the game
+ * Represents end page to be shown after the game is terminated or ended by one of the player.
  */
 @Component({
   selector: 'app-landing',
@@ -16,36 +13,19 @@ import { TranslateService } from '@ngx-translate/core';
 })
 export class LandingComponent implements OnInit {
 
-  winNotification: NotificationMessage;
-  looseNotification: NotificationMessage;
-
   constructor(
     private notificationService: NotificationService,
-    public translate: TranslateService) {
-
-      this.winNotification = {
-        message: 'Game over - you\'ve won!',
-        type: NotificationType.warning}
-      
-      this.looseNotification = {
-        message: 'Game over - you\'ve lost',
-        type: NotificationType.warning}
-      
-      translate.setDefaultLang('en');
-      const browserLang = translate.getBrowserLang();
-      translate.use(browserLang.match(/en|pl/) ? browserLang : 'en');
-   }
-
-  ngOnInit(): void {
-    this.showMessage(this.winNotification);
-    this.showMessage(this.looseNotification);
-  }
+    public translate: TranslateService) {}
 
   /**
-   * Displays toast notifications
+   * On page initialization prints toast notifications about winning/loosing in appropriate language.
    */
-  showMessage(message: NotificationMessage) {
-    this.notificationService.sendMessage(message);
+  ngOnInit(): void {
+    this.translate
+        .get('TOAST.WIN')
+        .subscribe((msg: string) => {this.notificationService.print(msg, NotificationType.warning)});
+    this.translate
+        .get('TOAST.LOOSE')
+        .subscribe((msg: string) => {this.notificationService.print(msg, NotificationType.warning)});
   }
-
 }
